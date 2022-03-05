3a4b4ff0ec012daa86a34165e476de5b3b0b4af0
anchalianaren <101001685+anchalianaren@users.noreply.github.com>
2022-03-05 20:09:32 +0530

    Update Q4_Code.sql

--- Q4_Code.sql
+++ Q4_Code.sql
@@ -1,2 +1,2 @@
-Q. In which county is the average age of mother the minimum and what is the minimum age?
+--Q. In which county is the average age of mother the minimum and what is the minimum age?
 select Ave_Age_Of_Mother, County_of_Residence from bigquery-public-data.sdoh_cdc_wonder_natality.county_natality where Ave_Age_Of_Mother = (select min (Ave_Age_Of_Mother)from bigquery-public-data.sdoh_cdc_wonder_natality.county_natality);
