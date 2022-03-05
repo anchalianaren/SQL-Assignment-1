c3d503f30d6bdbb55cbea0230abd94c9f1f8b9b1
anchalianaren <101001685+anchalianaren@users.noreply.github.com>
2022-03-05 20:12:03 +0530

    Update Q5_Code.sql

--- Q5_Code.sql
+++ Q5_Code.sql
@@ -1,1 +1,2 @@
-
+--Q. In which counties is the average age of mother less than 24 and they have congenital abnormalities?
+select distinct (County_of_Residence), Ave_Age_of_Mother from  bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_congenital_abnormalities where Ave_Age_of_Mother<=24 order by County_of_Residence asc 
